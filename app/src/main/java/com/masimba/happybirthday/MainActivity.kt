7c00ded8136a08431a55de15fde00ed1e6e75e94
mundembe <masimbamundembe@gmail.com>
2025-04-19 22:50:10 +0200

    Initial commit

--- app/src/main/java/com/masimba/happybirthday/MainActivity.kt
+++ app/src/main/java/com/masimba/happybirthday/MainActivity.kt
@@ -5,16 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.masimba.happybirthday.ui.theme.HappyBirthdayTheme
 
