2b7c116cb9eb502d32a9b52683684a5069c97c95
mundembe <masimbamundembe@gmail.com>
2025-04-19 22:49:15 +0200

    Initial commit

--- app/src/main/java/com/masimba/happybirthday/MainActivity.kt
+++ app/src/main/java/com/masimba/happybirthday/MainActivity.kt
@@ -35,7 +35,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview()
 @Composable
 fun GreetingText(message : String, from : String, modifier : Modifier = Modifier){
     Column (modifier = modifier) {
